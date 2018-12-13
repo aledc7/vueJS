e5c816af28e3f4ed22ceb5e1e7e9f8b5c73204ea
aledc7 <alejandrocdecastro@gmail.com>
2018-12-13 15:27:56 -0300

    completado capitulo 25

--- lesson22/src/App.vue
+++ lesson22/src/App.vue
@@ -1,112 +1,121 @@
 <template>
-<div id="app" class="container">
-        <h2>ABM DE TAREAS</h2>
+  <div id="app" class="container">
+    <h2>ABM DE TAREAS</h2>
 
-        <ul class="list-group tasks">
-            <app-task  v-for="(task, index) in tasks" :tasks="tasks" v-bind:key="task.id"
-            :task="task" :index="index" @remove="deleteTask"></app-task>
-        </ul>
 
-        
+    <app-task-list :tasks="tasks"> </app-task-list>
 
-        <form @submit.prevent="createTask" class="new-task-form">
-            <input v-model="new_task" type="text" class="form-control" placeholder="escriba una tarea">
-            <button class="btn btn-primary">Crear tarea</button>
-        </form>
-        <br>
-        <button v-on:click="deleteCompleted" class="btn btn-primary">Borrar completadas</button>
-        <ul>
-            <li v-for="error in errors">{{ error }}</li>
-        </ul>
+    <app-task-list :tasks="tasks2"> </app-task-list>
 
-        <footer class="footer">
-            <p>&copy; 2019 Ingenea SRL</p>
-        </footer>
-    </div> 
+    
+
+
+    <br>
+    <button v-on:click="deleteCompleted" class="btn btn-primary">Borrar completadas</button>
+    <ul>
+      <li v-for="error in errors">{{ error }}</li>
+    </ul>
+
+<h4>Crear</h4>
+
+<app-task-form @created="createTask"></app-task-form>
+
+<h4>Imprimir</h4>
+<app-task-form @created="alertTask"></app-task-form>
+
+
+
+    <footer class="footer">
+      <p>&copy; 2019 Ingenea SRL</p>
+    </footer>
+  </div>
 </template>
 
 <script>
-import Task from './Task.vue'
+import TaskList from './TaskList.vue';
+import TaskForm from './TaskForm.vue'
 
 export default {
-            components: {
-                'app-task': Task
-                        },
-            data(){ //data: function()
-              return {
-                errors: [],
-                new_task: null,
-                tasks: [{
-                        description: 'Primer Tarea',
-                        pending: true,
-                    },
-                    {
-                        description: 'Segunda Tarea',
-                        pending: true,
-                    },
-                    {
-                        description: 'Tercer Tarea',
-                        pending: true,
-                    }
-                ]
-            }},
-            // aca le pongo in id a cada tarea
-            created(){//created: function()
-                this.tasks.forEach((task,index) => this.$set(task,'id',index + 1));
+  components: {
+    'app-task-list': TaskList,
+    'app-task-form': TaskForm
+    
+  },
+  data() {
+    //data: function()
+    return {
+      errors: [],
+      new_task: null,
+      tasks: [
+        {
+          description: "Primer Tarea",
+          pending: true
+        },
+        {
+          description: "Segunda Tarea",
+          pending: true
+        },
+        {
+          description: "Tercer Tarea",
+          pending: true
+        }
+      ],
+      tasks2: [
+        {
+          description: "Nuevo Array",
+          pending: true
+        },
+        {
+          description: "Probando Otra tarea",
+          pending: true
+        },
+        {
+          description: "Claro que si",
+          pending: true
+        }
+      ]
+    };
+  },
+  // aca le pongo in id a cada tarea
+  created() {
+    //created: function()
+    this.tasks.forEach((task, index) => this.$set(task, "id", index + 1));
 
-                /* este codigo se reemplazo con 'arrow function'
-                this.tasks.forEach(function(task,index){
-                    this.$set(task,'id',index +1);
-                }.bind(this));
-                */
-            },
+ 
+  },
 
-            methods: {
-                deleteCompleted(){//deleteCompleted: function ()
+  methods: {
+    createTask(task){
+      this.tasks.push(task);
+    },
+    alertTask(task){
+      alert(task.description);
+    },
+    deleteCompleted() {
+      //deleteCompleted: function ()
 
-                    this.tasks = this.tasks.filter(task => task.pending);
+      this.tasks = this.tasks.filter(task => task.pending);
 
-                    /* modo viejo
+      /* modo viejo
                     this.tasks = this.tasks.filter(function (task) {
                         return task.pending;
                     })
 
                     */
+    },
 
-                },
+    
 
-                createTask(){ // createTask: function ()
-                    this.errors = [];
-                    if (!this.new_task) {
-                        this.errors.push('Debe escribir el nombre de la Tarea');
-                    } else {
-                        this.tasks.push({
-                            description: this.new_task,
-                            pending: true,
-                            editing: false
-                        });
-                        this.new_task = '';
-                    }
-                },
+    // ejemplo explicado del método 'splice' de javascript:
 
-                deleteTask(index){ //deleteTask: function(index)
-                    this.tasks.splice(index, 1);
-                }
+    //    defino un array con 6 elementos
+    //    var arr = new Array("1", "2", "3", "4", "5", "6");
 
+    //  quiero empezar en el elemento 2, quiero borrar 2 elementos y reemplazarlos con los otros dos elementos; (77 y 78)
+    //    arr.splice(2, 2, "77", "78");
 
-
-                // ejemplo explicado del método 'splice' de javascript:
-
-                //    defino un array con 6 elementos
-                //    var arr = new Array("1", "2", "3", "4", "5", "6");
-
-                //  quiero empezar en el elemento 2, quiero borrar 2 elementos y reemplazarlos con los otros dos elementos; (77 y 78)
-                //    arr.splice(2, 2, "77", "78");
-
-
-                //    document.write(arr);
-                // Output: 1,2,77,78,5,6
-
-            } // fin metodos
-        }
+    //    document.write(arr);
+    // Output: 1,2,77,78,5,6
+  } // fin metodos
+};
 </script>
