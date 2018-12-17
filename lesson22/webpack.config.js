76fcb972bd041584375d4aea23b6b7ea7c207c0b
aledc7 <alejandrocdecastro@gmail.com>
2018-12-17 09:33:03 -0300

    ok capitulo 31, minificando el css

--- lesson22/webpack.config.js
+++ lesson22/webpack.config.js
@@ -1,6 +1,10 @@
 var path = require('path')
 var webpack = require('webpack')
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
+var glob = require('glob');
+var PurifyCSSPlugin = require('purifycss-webpack');
+var inProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: './src/main.js',
@@ -45,6 +49,10 @@ module.exports = {
               use: 'css-loader!sass-loader',
               fallback: 'vue-style-loader'
               })
+          },
+          cssModules: {
+            localIdentName: '_module_[hash:base64]',
+            camelCase: true
           }
           // other vue-loader options go here
         }
@@ -64,7 +72,17 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin("style.css")
+    new ExtractTextPlugin("style.css"),
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    // Make sure this is after ExtractTextPlugin!
+    new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute!
+      paths: glob.sync(path.join(__dirname, 'src/*.vue')),
+      purifyOptions:{
+        minify: inProduction,
+        whitelist: ['*_module_*']
+      }
+    })
   ],
   
   resolve: {
@@ -84,7 +102,7 @@ module.exports = {
   devtool: '#eval-source-map'
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (inProduction) {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
